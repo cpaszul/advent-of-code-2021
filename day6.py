937e20bf5786a2255d759de8a81a9a9072d52fc2
Cory Paszul <cpaszul@gmail.com>
2021-12-06 08:52:00 -0500

    Improved performance (uses rotating deque, not counter)

--- day6.py
+++ day6.py
@@ -1,22 +1,18 @@
-from collections import Counter
+from collections import deque
 
 DEFAULT_INPUT = 'day6.txt'
 
 def day_6(loc: str = DEFAULT_INPUT) -> tuple[int, int]:
     with open(loc) as f:
-        fish = Counter(map(int, f.readline().split(',')))
+        fish = deque([0] * 9)
+        for n in map(int, f.readline().split(',')):
+            fish[n] += 1
     for i in range(256):
-        new_fish = Counter()
-        for turn, fish_num in fish.items():
-            if turn == 0:
-                new_fish[6] += fish_num
-                new_fish[8] += fish_num
-            else:
-                new_fish[turn - 1] += fish_num
-        fish = new_fish
+        fish[7] += fish[0]
+        fish.rotate(-1)
         if i == 79:
-            part_1_result = sum(fish.values())
-    return part_1_result, sum(fish.values())
+            part_1_result = sum(fish)
+    return part_1_result, sum(fish)
 
 if __name__ == '__main__':
     part_1, part_2 = day_6()
