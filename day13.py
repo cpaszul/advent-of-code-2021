3075b40f03aba6d48518574e56f556c0dd2120fb
Cory Paszul <cpaszul@gmail.com>
2021-12-13 07:54:59 -0500

    Improved day 13

--- day13.py
+++ day13.py
@@ -2,49 +2,47 @@ import re
 
 DEFAULT_INPUT = 'day13.txt'
 
-def day_13(loc: str = DEFAULT_INPUT) -> tuple[int, dict[tuple[int, int], bool]]:
-    grid = {}
+def day_13(loc: str = DEFAULT_INPUT) -> tuple[int, set[tuple[int, int]]]:
+    points = set()
     folds = []
     with open(loc) as f:
         for line in f.readlines():
             if m := re.match(r'(\d+),(\d+)', line):
                 x, y = map(int, (m.group(1), m.group(2)))
-                grid[(x, y)] = True
+                points.add((x, y))
             elif m := re.match(r'fold along (\w)=(\d+)', line):
                 folds.append((m.group(1), int(m.group(2))))
-    grid = fold(grid, *folds[0])
-    part_1_result = len(grid)
+    points = fold(points, *folds[0])
+    part_1_result = len(points)
     for f in folds[1:]:
-        grid = fold(grid, *f)
-    return part_1_result, grid
+        points = fold(points, *f)
+    return part_1_result, points
 
-def fold(grid: dict[tuple[int, int], bool], fold_dir: str, fold_line: int) -> None:
-    new_points = []
-    for key in grid:
-        if fold_dir == 'x' and key[0] > fold_line:
-            move_amount = key[0] - fold_line
-            new_points.append((fold_line - move_amount, key[1]))
-            grid[key] = False
-        if fold_dir == 'y' and key[1] > fold_line:
-            move_amount = key[1] - fold_line
-            new_points.append((key[0], fold_line - move_amount))
-            grid[key] = False
-    for point in new_points:
-        grid[point] = True
-    grid = {k:v for k, v in grid.items() if v}
-    return grid
+# move_amount = x - fold_line;new_x = fold_line - move_amount ->
+# new_x = fold_line - (x - fold_line) -> new_x = 2 * fold_line - x
+def fold(points: set[tuple[int, int]], fold_dir: str, fold_line: int) -> set[tuple[int, int]]:
+    new_points = set()
+    for point in points:
+        x, y = point
+        if fold_dir == 'x' and x > fold_line:
+            new_points.add((2 * fold_line - x, y))
+        elif fold_dir == 'y' and y > fold_line:
+            new_points.add((x, 2 * fold_line - y))
+        else:
+            new_points.add(point)
+    return new_points
 
-def draw(grid: dict[tuple[int, int], bool]) -> None:
-    max_x = max(grid, key=lambda p:p[0])[0]
-    max_y = max(grid, key=lambda p:p[1])[1]
+def draw(points: set[tuple[int, int]]) -> None:
+    max_x = max(points, key=lambda p:p[0])[0]
+    max_y = max(points, key=lambda p:p[1])[1]
     rows = []
     for y in range(max_y + 1):
         row = ''
         for x in range(max_x + 1):
-            if (x, y) in grid:
+            if (x, y) in points:
                 row += '#'
             else:
-                row += '.'
+                row += ' '
         rows.append(row)
     print('\n'.join(rows))
         
